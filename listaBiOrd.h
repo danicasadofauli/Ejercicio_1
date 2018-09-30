2b36407fda6f63dde3664b77c73b75f1505e9026
Daniel <daniel.cfauli@gmail.com>
2018-09-30 20:32:15 +0200

    feat: Defined functions that the list operates
    Defined all the functions of the ordered list, writing the respective
    headers in both .h and .c (this last one having the functions empty as a
    future template)

--- listaBiOrd.h
+++ listaBiOrd.h
@@ -13,3 +13,17 @@ typedef struct {
     Nodo *pdi
 } ListaBiOrd;
 
+ListaBiOrd LiSTABIORD_crea();
+void LISTABIORD_insertaOrd(ListaBiOrd *list, int elemento);
+int LISTABIORD_consulta(ListaBiOrd list);
+void LISTABIORD_elimina(ListaBi *list);
+void LISTABIORD_avanza (ListaBi *list);
+void LISTABIORD_retrocede (ListaBi *list);
+void LISTABIORD_vesInicio (ListaBi *list);
+void LISTABIORD_vesFinal (ListaBi *list);
+int LISTABIORD_inicio (ListaBi list);
+int LISTABIORD_final (ListaBi list);
+void LISTABIORD_destruye (ListaBi *list);
+
+#endif
+
