ed08f47d0943e32611f0f732302135d51ef08c54
Daniel <daniel.cfauli@gmail.com>
2018-09-30 19:46:18 +0200

    feat: Defined structure of the list
    Define structure of the ordered list by composition of nodes and a
    primary structure that unites all the nodes together (by keeping the
    location of the first and last node, as well as the node on which the
    user is located in the list.

--- listaBiOrd.h
+++ listaBiOrd.h
@@ -1,3 +1,15 @@
 #ifndef _LISTABIORD_H_
 #define _LISTABIORD_H_
 
+typedef struct N {
+    int elemento;
+    struct N *sig;
+    struct N *ant;
+} Nodo;
+
+typedef struct {
+    Nodo *pri;
+    Nodo *ult;
+    Nodo *pdi
+} ListaBiOrd;
+
