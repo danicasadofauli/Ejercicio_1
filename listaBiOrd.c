64c35fb0f22991d65fb5afbff1f6418706007deb
Daniel <daniel.cfauli@gmail.com>
2018-10-06 18:51:35 +0200

    doc: Included information of functions' functionality
    Added information to each function so it includes it's purpose, what
    variables are used in the context and the output parameters.

--- listaBiOrd.c
+++ listaBiOrd.c
@@ -1,5 +1,13 @@
 #include "listaBiOrd.h"
 
+/*********************************************************************************************************************
+*
+* @Finalidad: Crea una nueva lista bidireccional ordenada vacía para que el usuario pueda rellenar posteriormente
+* @Parametros: xxxxxxx
+* @Retorno: Devuelve la lista vacía creada
+*
+**********************************************************************************************************************/
+
 ListaBiOrd LiSTABIORD_crea(){
     ListaBiOrd list;
 
@@ -27,6 +35,16 @@ ListaBiOrd LiSTABIORD_crea(){
 
     return list;
 }
+
+/*********************************************************************************************************************
+*
+* @Finalidad: Inserta un elemento de valor entero que el usuario determine dentro de la lista de forma que esté
+*             ordenado de menor a mayor
+* @Parametros:	*list = puntero que contiene la dirección donde se encuentra la lista bidireccional ordenada
+*				elemento = variable entera que el usuario quiere insertar en la lista
+* @Retorno: xxxxxxxxx
+*
+**********************************************************************************************************************/
 
 void LISTABIORD_insertaOrd(ListaBiOrd *list, int elemento){
     Nodo *aux;
@@ -64,6 +82,15 @@ void LISTABIORD_insertaOrd(ListaBiOrd *list, int elemento){
     }
 }
 
+/*********************************************************************************************************************
+*
+* @Finalidad: Devuelve el elemento que se encuentre en el nodo al que apunta la PDI de la lista
+* @Parametros:	list = estructura de punteros que apuntan a diferentes nodos principales de la lista (principio,
+*               final y PDI)
+* @Retorno: Devuelve el valor del elemento consultado (elemento en PDI)
+*
+**********************************************************************************************************************/
+
 int LISTABIORD_consulta(ListaBiOrd list){
     int elemento;
 
@@ -79,6 +106,14 @@ int LISTABIORD_consulta(ListaBiOrd list){
 
     return elemento;
 }
+
+/*********************************************************************************************************************
+*
+* @Finalidad: Elimina de la lista el nodo al que PDI esté apuntando
+* @Parametros:	*list = puntero que contiene la dirección donde se encuentra la lista bidireccional ordenada
+* @Retorno: xxxxxxxxx
+*
+**********************************************************************************************************************/
 
 void LISTABIORD_elimina(ListaBi *list){
     Nodo *aux;
@@ -97,6 +132,14 @@ void LISTABIORD_elimina(ListaBi *list){
     }
 }
 
+/*********************************************************************************************************************
+*
+* @Finalidad: Mueve el puntero PDI a la casilla siguiente de la lista con respecto a la que se encuentra.
+* @Parametros:	*list = puntero que contiene la dirección donde se encuentra la lista bidireccional ordenada
+* @Retorno: xxxxxxxxx
+*
+**********************************************************************************************************************/
+
 void LISTABIORD_avanza (ListaBi *list){
     if (lista -> pdi == lista -> ult){
         printf("Error al avanzar en la lista");
@@ -105,6 +148,14 @@ void LISTABIORD_avanza (ListaBi *list){
         lista -> pdi = lista-> pdi -> sig;
     }
 }
+
+/*********************************************************************************************************************
+*
+* @Finalidad: Mueve el puntero PDI a la casilla anterior de la lista con respecto a la que se encuentra.
+* @Parametros:	*list = puntero que contiene la dirección donde se encuentra la lista bidireccional ordenada
+* @Retorno: xxxxxxxxx
+*
+**********************************************************************************************************************/
 
 void LISTABIORD_retrocede (ListaBi *list){
     if (lista -> pdi == lista -> pri){
@@ -115,23 +166,76 @@ void LISTABIORD_retrocede (ListaBi *list){
     }
 }
 
+/*********************************************************************************************************************
+*
+* @Finalidad: Mueve el puntero PDI a la primera casilla de la lista (no fantasma)
+* @Parametros:	*list = puntero que contiene la dirección donde se encuentra la lista bidireccional ordenada
+* @Retorno: xxxxxxxxx
+*
+**********************************************************************************************************************/
+
 void LISTABIORD_vesInicio (ListaBi *list){
     lista -> pdi = lista -> pri -> sig;
 }
+
+/*********************************************************************************************************************
+*
+* @Finalidad: Mueve el puntero PDI a la última casilla de la lista (no fantasma)
+* @Parametros:	*list = puntero que contiene la dirección donde se encuentra la lista bidireccional ordenada
+* @Retorno: xxxxxxxxx
+*
+**********************************************************************************************************************/
 
 void LISTABIORD_vesFinal (ListaBi *list){
     lista -> pdi = lista -> ult -> ant;
 }
 
+/*********************************************************************************************************************
+*
+* @Finalidad: Mueve el puntero PDI a la primera casilla de la lista (no fantasma). Usado para finalizar recorridos
+*             por la lista de final a principio.
+* @Parametros:	list = estructura de punteros que apuntan a diferentes nodos principales de la lista (principio,
+*               final y PDI)
+* @Retorno: Cierto (1) si PDI se encuentra en la primera casilla fantasma, falso (0) si no
+*
+**********************************************************************************************************************/
+
 int LISTABIORD_inicio (ListaBi list){
     return lista.pdi == lista.pri;
 }
+
+/*********************************************************************************************************************
+*
+* @Finalidad: Mueve el puntero PDI a la última casilla de la lista (no fantasma). Usado para finalizar recorridos
+*             por la lista de principio a final.
+* @Parametros:	list = estructura de punteros que apuntan a diferentes nodos principales de la lista (principio,
+*               final y PDI)
+* @Retorno: Cierto (1) si PDI se encuentra en la primera casilla fantasma, falso (0) si no
+*
+**********************************************************************************************************************/
 
 int LISTABIORD_final (ListaBi list){
     return  lista.pdi == lista.ult;
 }
 
+/*********************************************************************************************************************
+*
+* @Finalidad: Destruye la lista al completo, haciendola inutilizable de nuevo y perdiéndose todos sus datos.
+* @Parametros:	*list = puntero que contiene la dirección donde se encuentra la lista bidireccional ordenada
+* @Retorno: xxxxxxxxx
+*
+**********************************************************************************************************************/
+
 void LISTABIORD_destruye (ListaBi *list){
-    
+    Nodo *aux;
+
+    while (lista -> pri != NULL){
+        aux = lista -> pri;     //Se asigna la dirección de pri a aux para guardar direccion
+        lista -> pri = lista -> pri -> sig;     //Avanzamos pri al sigueinte elemento
+        free(aux);  //Eliminamos el nodo que habíamos guardado en aux
+    }
+
+    lista -> ult = NULL;
+    lista -> pdi = NULL;
 }
 
